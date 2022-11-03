69d5b072861619b364e454710359772e4e268f5f
Bruno Cavazza <30497134+BrunoCavazza@users.noreply.github.com>
2022-11-02 21:11:05 -0300

    Entrega 3/11

--- src/Main.java
+++ src/Main.java
@@ -1,41 +1,52 @@
-import Funciones.CrearNodo;
-import com.clases.clase4.Node;
-import com.clases.clase4.Avl;
 import java.util.Scanner;
 
+import Nodo.Avl;
+import Nodo.Node;
+import Funciones.CrearNodo;
+import java.io.PrintStream;
+
 public class Main {
-    public static void main(String[] args) {
-        Node nodo = new Node();
+    public static void main(String[] args) throws Exception {
+
+        Node<Integer> nodo = new Node<>();
+        Avl<Integer> avl = new Avl<>();
         int opcion;
-        Avl avl;
         Scanner scanner = new Scanner(System.in);
+
+
         System.out.print("Ingrese primer nodo (a preguntar despues): ");
-        nodo.addFirstValue(scanner.nextInt());
         System.out.println("Ingrese opcion de menu:");
         do{
-            System.out.println(" 1- Agregar nodos por teclado\n 2- Agregar nodos aleatorio\n 3- Mostrar arbol ordenado\n 4- Mostrar arbol con formato\n 5- Agregar nodos aleatorio AVL\n 6- Agregar nodos manual AVL\n 0- Salir");
+            System.out.println(" 1- Agregar nodos por teclado\n 2- Agregar nodos aleatorio\n 3- Mostrar arbol ordenado\n 4- Mostrar arbol con formato\n 5- Agregar nodos aleatorio AVL\n 6- Agregar nodos manual AVL\n 7- Ordenados avl\n 8- Dibujo Avl\n 0- Salir");
             opcion = scanner.nextInt();
+
             switch(opcion){
                 case 1:
                 case 2:
                     CrearNodo.creadorNodos(opcion, nodo);
                     break;
                 case 3:
-                    nodo.printInOrderBinaryTree();
+                    nodo.printInOrder();
                     break;
                 case 4:
-                    nodo.printAsTree();
+                    nodo.printTree(nodo);
                     break;
                 case 5:
-                    avl = nodo.randomAVL();
+                    avl = CrearNodo.randomAVL();
                     break;
                 case 6:
-                    avl = nodo.manualAVL();
+                    avl = CrearNodo.manualAVL();
+                    break;
+                case 7:
+                    avl.printInOrderAvl();
+                    break;
+                case 8:
+                    avl.printTreeAvl(new PrintStream(System.out));
                     break;
                 case 0:
                     break;
                 default:
-                    System.out.println("Dale pibe poneme bien los numeritos.");
+                    System.out.println("Dale pibe pone bien los numeritos.");
                     break;
             }
         }while (opcion!=0);
